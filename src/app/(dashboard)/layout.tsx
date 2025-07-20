52acdde6d32338d7fdcd63a054189b043e29c787
Navid Talebi <navid.talebe@gmail.com>
2025-07-20 12:24:53 +0330

    feat: add dashboard

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import DashboardSidebar from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import DashboardNavbar from "@/modules/dashboard/ui/components/dashboard-navbar";
+
+const Layout = ({children}:PropsWithChildren)=>{
+
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
+
+export default Layout
---
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -0,0 +1,26 @@
+import { Dispatch, SetStateAction } from "react";
+
+import {
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandDialog,
+} from "@/components/ui/command";
+
+interface Props {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const DashboardCommand = ({ open, setOpen }: Props) => {
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen}>
+      <CommandInput placeholder="find a meeting or agent  " />
+      <CommandList>
+        <CommandItem>Test</CommandItem>
+      </CommandList>
+    </CommandDialog>
+  );
+};
+
+export default DashboardCommand;
